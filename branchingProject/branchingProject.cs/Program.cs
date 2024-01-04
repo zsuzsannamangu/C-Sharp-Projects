96eeeab1ba31f37d203ba8ff40f3a33d53ca5794
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-04 15:10:53 -0800

    Condensed code

---
+++ branchingProject/branchingProject.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace branchingProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            if (name == "Erik")
+            {
+                Console.WriteLine("Your name is Erik.");
+            }
+            else if (name == "Sandra")
+            {
+                Console.WriteLine("Your name is Sandra.");
+            }
+            else
+            {
+                Console.WriteLine("You are not Sandra or Erik.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
