004d0f62f22a1f6110e2c71b9a4ff8a4ae62f073
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-04 18:34:23 -0800

    Added functions to strings

---
+++ stringsPractice/stringsPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace stringsPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name1 = "Jarred";
+            string name2 = "Jason";
+            string name3 = "Smith";
+            string newName1 = name1.ToUpper(); //Converts a string to uppercase.
+            string result = newName1 + " " + name2 + " " + name3; //Concatenates three strings.
+            Console.WriteLine(result);
+
+            StringBuilder myStringBuilder = new StringBuilder("The object is immutable. "); //Creates a Stringbuilder and builds a paragraph of text, one sentence at a time.
+            myStringBuilder.Append("Every time you want to modify it, you create a new string object in memory. ");
+            myStringBuilder.Insert(4, "String "); //Inserts a word into the fourth position of a StringBuilder object.
+            myStringBuilder.Append("This requires a new allocation of space for that new object. That's why we use StringBuilders.");
+            Console.WriteLine(myStringBuilder);
+
+            Console.Read();
+        }
+    }
+}
