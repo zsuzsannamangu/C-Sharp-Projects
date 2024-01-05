9897e32d05e3423ed4e56fa6e2c9027551ed27b0
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-04 16:55:53 -0800

    Added branching project
    This project calculates package shipping costs.

--- branchingProject/branchingProject.cs/Program.cs
+++ branchingProject/branchingProject.cs/Program.cs
@@ -6,21 +6,32 @@ namespace branchingProject
     {
         static void Main()
         {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            if (name == "Erik")
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Enter package weight in lbs:");
+            int packageWeight = Convert.ToInt32(Console.ReadLine()); //creates integer variable which holds the user input
+            if (packageWeight > 50) //if the weight of the package is greater than 50, display the message inside of the {}
             {
-                Console.WriteLine("Your name is Erik.");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day!");
             }
-            else if (name == "Sandra")
+            else //if it's less than 50, do the following:
             {
-                Console.WriteLine("Your name is Sandra.");
+                Console.WriteLine("Enter package width");
+                int packageWidth = Convert.ToInt32(Console.ReadLine()); //this integer holds the the width that user entered
+                Console.WriteLine("Enter package height");
+                int packageHeight= Convert.ToInt32(Console.ReadLine());//this integer holds the height that user entered
+                Console.WriteLine("Enter package length");
+                int packageLength = Convert.ToInt32(Console.ReadLine());//this integer holds the length that user entered
+                if (packageWidth * packageLength * packageHeight > 50) //if the total dimensions are greater than 50, display message below in {}
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day!");
+                }
+                else //otherwise display the quote:
+                { 
+                    float quote = (packageWeight * (packageWidth * packageLength * packageHeight)) / 100f; //float is used here instead of integer, since integer can only be whole numbers
+                    Console.WriteLine("Your quote is $" + quote + ".");//this message is displayed to the user.
+                }
             }
-            else
-            {
-                Console.WriteLine("You are not Sandra or Erik.");
-            }
-            Console.ReadLine();
+                Console.ReadLine(); //this prevents the window from closing automatically
         }
     }
 }
