7b4e05e08b39255d98fb61baa3e8a2a8645e961b
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-10 13:08:53 -0800

    Revised assignment 4,5,6

--- iterationAssignment/iterationAssignment.cs/Program.cs
+++ iterationAssignment/iterationAssignment.cs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 namespace iterationAssignment.cs
 {
@@ -75,9 +76,18 @@ namespace iterationAssignment.cs
             //Part Four Assignment
             Console.WriteLine("\nPart 4:");
 
+            //Create a list of strings where each item in the list is unique. Ask the user to input text to search for in the list.
+            //Create a loop that iterates through the list and then displays the index of the list item that contains matching text on the screen.
+            //Check if the user put in text that isn't on the list and, if they did, tell the user their input is not on the list.
+            //Stop the loop from executing once a match has been found.
+
             List<string> names = new List<string>() { "Jesse", "Bob", "Kristi", "Leo" }; //"names" is a list of strings
-            Console.WriteLine("Search for a name in the list:"); //user is prompted to enter a name
-            string nameSearch = Console.ReadLine(); //the user entry is saved in the string "nameSearch"
+            Console.WriteLine("Search for a name on the list:"); //user is prompted to enter a name
+
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo; //create a TextInfo object for capitalizing the user entry, the TextInfo class is part of the System.Globalization namespace
+            string nameSearch = textInfo.ToTitleCase(Console.ReadLine()); //the user entry is capitalized with the ToTitleCase method of the TextInfo class and saved in the string "nameSearch"
+                                                                          //The ToTitleCase method converts the first character of each word in a string to uppercase, and the rest to lowercase.
+            Console.WriteLine("You are searching for: " + nameSearch);
 
             int index = -1; //start with index at -1 which is equivalent to "not found" since there is no index -1
             for (int k = 0; k < names.Count; k++) //for loop
@@ -91,25 +101,26 @@ namespace iterationAssignment.cs
 
             if (index == -1) //if name hasn't been found
             {
-                Console.WriteLine("That name is not in the list."); //write this message
+                Console.WriteLine("That is not on the list."); //write this message
             }
             else //otherwise, if name has been found, write this message:
             {
-                Console.WriteLine("That name is found at index " + index);
+                Console.WriteLine("That name is found at index " + index + ".");
             }
 
             //Solution without a foor loop:
-                //bool searchResult = names.Contains(nameSearch);
-                //if (searchResult == true)
-                //{
-                //    Console.WriteLine("That name is in the list.");
-                //    int index = names.IndexOf(nameSearch);
-                //    Console.WriteLine("The index is: " + index);
-                //}
-                //else
-                //{
-                //    Console.WriteLine("That name is not in the list.");
-                //}
+            //Although this will treat pineapple and apple the same if the search is for apple
+            //bool searchResult = names.Contains(nameSearch);
+            //if (searchResult == true)
+            //{
+            //    Console.WriteLine("That name is on the list.");
+            //    int index = names.IndexOf(nameSearch);
+            //    Console.WriteLine("The index is: " + index);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("That name is not on the list.");
+            //}
 
 
 
@@ -117,67 +128,60 @@ namespace iterationAssignment.cs
             //Part Five Assignment
             Console.WriteLine("\nPart 5:");
 
+            //Create a list of strings that has at least two identical strings in the list. Ask the user to select text to search for in the list.
+            //Create a loop that iterates through the list and then displays the indices of the items matching the user-selected text.
+            //Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list.
+            //(It does not provide any additional chances for the user to enter text.)
+
             List<string> fruits = new List<string>() { "papaya", "orange", "banana", "apple", "apple", "blueberry", "orange", "mango", "blueberry", "orange" }; //"fruits" is a list of strings
 
-            Console.WriteLine("Search for a fruit in the list:"); //user is prompted to enter a fruit
+            Console.WriteLine("List of fruits: " + string.Join(" ", fruits));
+            Console.WriteLine("Select text to search for in the list: "); //user is prompted to select a fruit
             string fruitSearch = Console.ReadLine(); //user input is saved in the "fruitSearch" string
-                                                     
-            int count = fruits.Where(x => x.Equals(fruitSearch)).Count(); //it counts how many times the word that the user entered appears in the list, that number is saved in the "count" variable
+
+            int count = fruits.Where(x => x.Equals(fruitSearch)).Count(); //it counts how many times the word that the user entered appears on the list, that number is saved in the "count" variable
                                                                           //to be able to use Where() you need to add "using System.Linq;"
-            //for (int i = 0; i < fruits.Count; i++) //it loops through the list as many times as many strings there are in the list
-            //{
-            //    if (fruits[i] == fruitSearch) //if the user input equals any words in the list, do this:
-            //    {
-            //        Console.WriteLine("The fruit " + fruitSearch + " appears " + count + " times in the list. The index is: " + i);
-            //    }
-            //    else //otherwise do this:
-            //    {
-            //        Console.WriteLine("There is no match.");
-            //    }
-            //}
 
-            //List<string> newListFruits = new List<string>();
-            //int place = -1;
+            List<int> countIndexes = new List<int>(); //this is an empty list to save into the indices of strings later
 
-            //for (int c = 0; c < fruits.Count; c++)
-            //{
-            //    bool fruitFound = fruits[c] == fruitSearch;
-            //    Console.WriteLine("bool fruitFound: " + fruitFound);
+            for (int i = 0; i < fruits.Count; i++) //it loops through the list as many times as many strings there are on the list
+            {
+                if (fruits[i] == fruitSearch && count == 1) //if the user input equals any words on the list and that word only occurs once, do this:
+                {
+                    Console.WriteLine("The fruit " + fruitSearch + " was found at index " + i + "."); //display this message, which has the name of the fruit and its index
+                    break; //exit the loop
+                }
+                else if (fruits[i] == fruitSearch && count > 1) //if the user input has been found on the list multiple times, do:
+                {
+                    countIndexes.Add(i); //add the indices of that string into the countIndexes list
+                }
+            }
 
-            //    if (fruitFound == true)
-            //    {
-            //        newListFruits.Add(fruits[c]);
+            if (count == 0) //if user input hasn't been found
+            {
+                Console.WriteLine("That is not on the list."); //write this message
+            }
 
-            //        if (newListFruits.Count == count)
-            //        {
-            //            Console.WriteLine("The fruit " + fruitSearch + " appears " + count + " times in the list. the index is: " + c);
-            //            break;
-            //        }
-            //    }
-            //}
+            else if (countIndexes.Count > 1) //else if that fruit has been found multiple times, because the list countIndexes have multiple numbers in it
+            {
+                Console.WriteLine("The fruit " + fruitSearch + " is on the list multiple times."); //display this message
+                Console.WriteLine("Its indices are: " + string.Join(" ", countIndexes)); //and then show this message with the indices displaying horizontally
+            }
 
-            //if (place == -1) //if fruit hasn't been found
-            //{
-            //    Console.WriteLine("That fruit is not in the list."); //write this message
-            //}
-            //else //otherwise, if fruit has been found, write this message:
-            //{
-            //    Console.WriteLine("That fruit is found at index");
-            //}
 
 
 
             //Part Six Assignment
             Console.WriteLine("\nPart 6:");
 
-            //Create a foreach loop that evaluates each item in the list, and displays a message showing the string
-            //and whether or not it has already appeared in the list.
+            //Create a foreach loop that evaluates each item on the list, and displays a message showing the string
+            //and whether or not it has already appeared on the list.
 
             List<string> veggies = new List<string>() { "Spinach", "Celery", "Spinach", "Cucumber", "Squash", "Celery", "Asparagus", "Cucumber", "Kale", "Celery" }; //"veggies" is a list of strings
 
             HashSet<string> newList = new HashSet<string>(); //using HashSet<> here instead of List as HashSet can only contain unique elements, so inserting duplicates are prevented automatically
                                                 //the variable "set" is a HashSet = an unordered collection of unique strings
-            foreach (string veggie in veggies) //a foreach loop will iterate through the veggies list, we are referring to each item in the list with the word "veggie"
+            foreach (string veggie in veggies) //a foreach loop will iterate through the veggies list, we are referring to each item on the list with the word "veggie"
             {
                 if (!newList.Add(veggie)) //newList.Add(veggie) adds all items to the HashSet only once, each appears only once, so then
                                             //!newList.Add(veggie) has all other items in order of appearance: spinach, celery, cucumber, celery -> celery appeard 3x
@@ -185,7 +189,7 @@ namespace iterationAssignment.cs
                 {
                     Console.WriteLine(veggie + " appeared more than once.");
                 }
-                else //otherwise, it is it's first appearance in the list
+                else //otherwise, it is it's first appearance on the list
                 {
                     Console.WriteLine(veggie + " appears for the first time.");
                 }
