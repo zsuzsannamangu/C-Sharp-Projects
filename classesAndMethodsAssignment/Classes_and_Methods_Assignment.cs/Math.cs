c3c8aa850496c8518b0f4ad02d23f7f0071e0fce
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-10 15:41:12 -0800

    Added "Classes and Methods" assignment
    This assignment has math operations.

---
+++ classesAndMethodsAssignment/Classes_and_Methods_Assignment.cs/Math.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Classes_and_Methods_Assignment.cs
+{
+    public class Math //A public class is accessible to all parts of the program
+    {
+        public static int Addition(int userNum) //Addition is a method of the Math class and it takes one parameter: the integer userNum. The value of userNum will be at Program.cs.
+        {
+            return 5 + userNum; //Addition will return an integer that is 5 + userNum. userNum is the number the user entered.
+        }
+
+        public static int Subtraction(int userNum)
+        {
+            return 10 - userNum; //The Subtraction method will return 10 minus the user entry: userNum
+        }
+
+        public static int Multiplication(int userNum)
+        {
+            return 2 * userNum; //The Multiplication method will return 2 times the user entry: userNum
+        }
+    }
+}
---
+++ classesAndMethodsAssignment/Classes_and_Methods_Assignment.cs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment.cs
+{
+    class Program
+    {
+        //Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer.
+        //The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
+        
+        //In the Main() program, ask the user what number they want to do the math operations on.
+        //Call each method in turn, passing the user input to the method.
+        //Display the returned integer to the screen.
+
+        static void Main()
+        {
+            bool validNum = false; //Start with a boolean variable that is false and will change to true if the user number is a valid number
+            while (!validNum) //While the number is valid, do the code in the {}. The while loop will allow the user to try again if the input wasn't valid
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number: "); //Prompts the user to enter a number
+                    int userNum = Convert.ToInt32(Console.ReadLine()); //User entry is stored in the userNum variable which is an integer
+
+                    Console.WriteLine("5 + " + userNum + " = " + Math.Addition(userNum)); //We are calling the Math class' Addition, Subtraction and Multiplication method which are at Math.cs
+                    Console.WriteLine("10 - " + userNum + " = " + Math.Subtraction(userNum));
+                    Console.WriteLine("2 * " + userNum + " = " + Math.Multiplication(userNum));
+
+                    validNum = true; //validNum boolean variable will change to true if the user entry was valid
+                    Console.ReadLine();
+                }
+
+                catch (FormatException ex) //This error message appears if the user enters something other than a number
+                {
+                    Console.WriteLine("Please enter a whole number!");
+                }
+
+                catch (Exception ex) //This is a general error message for another issue not stated above
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                finally //The finally block runs no matter what and it typically has a database log
+                {
+                    Console.WriteLine("Thank you!");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
