1b759149eb091b88a3d7acb7987ecbb0f37c4809
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-11 13:24:38 -0800

    Added void method

---
+++ Method_Class_Assignment.cs/Method_Class_Assignment.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Method_Class_Assignment.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Subtraction subtractionObject = new Subtraction();
+
+            Console.WriteLine("Version 1:");
+            subtractionObject.Subtract(2, 3);
+            Console.WriteLine("Version 2:");
+            subtractionObject.Subtract(num1: 2, num2: 3);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method_Class_Assignment.cs/Method_Class_Assignment.cs/Subtraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Method_Class_Assignment.cs
+{
+    class Subtraction
+    {
+        public void Subtract(int num1, int num2)
+        {
+            int num3 = 10 - num1;
+            Console.WriteLine("10 - 2 is " + num3);
+            Console.WriteLine("The second number is " + num2);
+        }
+
+    }
+}
