7d28f3e1840ae15c97c04e56d5036a3a5901f9b6
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-11 11:25:22 -0800

    Added assignment with math operations
    Using methods. Math operations with different datatypes.

---
+++ Main_Method_Assignment/Main_Method_Assignment.cs/Operations.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Main_Method_Assignment.cs
+{
+    class Operations
+    {
+        //We defined 3 versions of the MathOperations method.
+        //C# allows to use the same name for methods that are similar as long as they are somewhat different -> they all take in a different parameter.
+        public static int MathOperations(int number) //The first MathOperations method takes an integer as a parameter
+        {
+            return 12 - number; //number is defined at Program.cs
+        }
+        public static decimal MathOperations(decimal decNumber) //The second MathOperations method takes a decimal (a number that can be up to 29 digits in length) as a parameter
+        {
+            return 2 * decNumber; //decNumber is defined at Program.cs
+        }
+        public static string MathOperations(string stringNumber) //The third MathOperations method takes a string as a parameter
+        {
+            return 12 + stringNumber; //stringNumber is defined at Program.cs
+        }
+
+    }
+}
---
+++ Main_Method_Assignment/Main_Method_Assignment.cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Main_Method_Assignment.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            //we are callig the MathOperations method 3 times which is at Operations.cs:
+
+            int number = 3;
+            Console.WriteLine("12 - " + number + " = " + Operations.MathOperations(number));
+
+            decimal decNumber = 9.11m; //decNumber is a decimal datatype which requires an "m" suffix after the value
+            int decNumberInt = Convert.ToInt32(Operations.MathOperations(decNumber)); //we are converting the result of the math operation from decimal to integer
+            Console.WriteLine("2 * " + decNumber + " = " + decNumberInt);
+
+            string stringNumber = "5"; //stringNumber is a string datatype so we use double quotes
+            Console.WriteLine("2 + " + stringNumber + " = " + Operations.MathOperations(Convert.ToInt32(stringNumber))); //we are converting string to integer in order to perform the math operation
+
+            Console.ReadLine(); //Console.ReadLine() prevents the console window from closing automatically
+        }
+    }
+}
