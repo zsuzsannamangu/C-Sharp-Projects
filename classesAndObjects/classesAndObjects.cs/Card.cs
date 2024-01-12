6837cdd380df850f2c1620d4f9ef3fb530bc9a27
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-12 11:59:35 -0800

    updates

--- classesAndObjects/classesAndObjects.cs/Card.cs
+++ classesAndObjects/classesAndObjects.cs/Card.cs
@@ -13,10 +13,19 @@ namespace classesAndObjects.cs
             Suit = "Spades"; //these are the default values
             Face = "Two";
         }
-        public string Suit { get; set; } //Public means that it's accessible to all parts of the program. Suit is the name with string datatype.
+        public Suit Suit { get; set; } //Public means that it's accessible to all parts of the program. Suit is the name with string datatype.
                                          //Suit is a property of the card class
                                          // get; set; are the 2 only two things you can do with an object property - you can get or set this property
         public string Face { get; set; }
+
+        public enum Suit
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades
+        }
+
     }
-    
+
 }
