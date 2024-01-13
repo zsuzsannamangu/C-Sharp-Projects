335d26998584d50b0315f62cb1e72e62dc4536f4
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-13 10:43:29 -0800

    Updated and finalized it

--- enum/enum.cs/Program.cs
+++ enum/enum.cs/Program.cs
@@ -1,82 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace @enum.cs
 {
     public class Program
     {
-        public enum DaysOfWeek
-        {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
-        }
-        static void Main()
-        {
-            
+        //Instructions for the assignment:
         //Create an enum for the days of the week.
         //Prompt the user to enter the current day of the week.
         //Assign the value to a variable of that enum data type you just created.
         //Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.
-
-        bool dayValid = false;
-            while (!dayValid)
+        static void Main()
+        {
+        bool dayValid = false; //we create a boolean variable dayValid and set it's initial value to false, it will change into true at the end of the try block
+            while (!dayValid) //while the dayValid variable is true, which is to say that while the user entry is a valid day of the week, do this:
             {
                 try
                 {
-                    // Get currrent day of week.
-                    DayOfWeek today = DateTime.Today.DayOfWeek;
-                    Console.WriteLine("Today is {0}", today);
+                    //DayOfWeek is a built-in enum that specifies the day of the week (use System to use it)
+                    DayOfWeek today = DateTime.Today.DayOfWeek; // Gets currrent day of week
 
-                    //Prints out the underlying value number of each day:
-                    foreach (string day in Enum.GetNames(typeof(DayOfWeek)))
-                    {
-                        Console.WriteLine("{0} = {1:D}", day,
-                                                     Enum.Parse(typeof(DayOfWeek), day));
-                    }
-                    //Adds an extra line:
-                    Console.WriteLine();
-
-                    DayOfWeek today2 = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), "Monday");
-                    Console.WriteLine("The orange value {0:D} has the combined entries of {0}",
-                                       today2);
-
-                    Console.WriteLine("Enter the current day of the week: ");
-                    string userInput = Console.ReadLine();
-                    Console.WriteLine("Your entry is {0}", userInput);
-
-                    DaysOfWeek daysofweek = new DaysOfWeek();
-
-                    // Test current day of week.
-                    //if (today == userInput)
+                    //Prints out the values in DayOfWeek:
+                    //foreach (string day in Enum.GetNames(typeof(DayOfWeek))) //GetNames returns an array of the names of the constants in an enumeration, so  returns what is in enum
                     //{
-                    //    Console.WriteLine("DO WORK");
+                    //    DayOfWeek days = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day);
+                    //    Console.WriteLine(days); 
                     //}
 
-                    
-                   
+                    Console.WriteLine("Enter the current day of the week: "); //Prompts user to enter today's day
+                    string userInput = Console.ReadLine(); //save user input in a string
 
+                    //Parse(): Converts a string to an enum, returns enum
+                    //Parse syntax: object Enum.Parse(Type enumtype, string value) (+1 overload)
 
+                    DayOfWeek userInputEnum = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), userInput); //Converts user input string type into enum type
 
+                    if (today == userInputEnum) //if user input equals today's date, print out this:
+                    {
+                        Console.WriteLine("You are correct. Today is {0}.", today);
+                    }
 
-                    dayValid = true;
+                    else if (today != userInputEnum) //if user input does not equal today's date, print out this:
+                    {
+                        Console.WriteLine("You are incorrect. Today is not {0}, it is {1}.", userInputEnum, today);
+                    }
+
+                    dayValid = true; //if user entry was a valid day of the week, dayValid boolean changes to true and it jumps to the finally block
                 }
-
-                catch (FormatException ex) //This error message appears if the user enters something other than a number
-                {
-                    Console.WriteLine("Please enter a whole number!");
-                }
-
                 catch (Exception ex) //This is a general error message for another issue not stated above
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Please enter an actual day of the week! Press enter to try again!");
                 }
 
                 finally //The finally block runs no matter what and it typically has a database log
@@ -84,45 +56,8 @@ namespace @enum.cs
                     Console.WriteLine("Thank you!");
                 }
                 Console.ReadLine();
-
             }
             
         }
     }
 }
-
-
-/*
-public class ParseTest
-{
-    [Flags]
-    enum Colors { Red = 1, Green = 2, Blue = 4, Yellow = 8 };
-
-    public static void Main()
-    {
-        Console.WriteLine("The entries of the Colors enumeration are:");
-        foreach (string colorName in Enum.GetNames(typeof(Colors)))
-        {
-            Console.WriteLine("{0} = {1:D}", colorName,
-                                         Enum.Parse(typeof(Colors), colorName));
-        }
-        Console.WriteLine();
-
-        Colors orange = (Colors)Enum.Parse(typeof(Colors), "Red, Yellow");
-        Console.WriteLine("The orange value {0:D} has the combined entries of {0}",
-                           orange);
-    }
-}
-
-
-This code example produces the following results:
-
-The entries of the Colors Enum are:
-Red = 1
-Green = 2
-Blue = 4
-Yellow = 8
-
-The orange value 9 has the combined entries of Red, Yellow
-
-*/
