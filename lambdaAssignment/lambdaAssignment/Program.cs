115cf15bbeb1465139ff67aa579ec4f3982da8c6
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-15 12:29:55 -0800

    Simplified code

--- lambdaAssignment/lambdaAssignment/Program.cs
+++ lambdaAssignment/lambdaAssignment/Program.cs
@@ -15,43 +15,63 @@ namespace lambdaAssignment
     {
         static void Main()
         {
-            Employee employee1 = new Employee() { Id = 1, FirstName = "Joe", LastName = "Smith" };
-            Employee employee2 = new Employee() { Id = 2, FirstName = "Sarah", LastName = "Holsey" };
-            Employee employee3 = new Employee() { Id = 3, FirstName = "Lili", LastName = "Trenchard" };
-            Employee employee4 = new Employee() { Id = 4, FirstName = "Marian", LastName = "Cronen" };
-            Employee employee5 = new Employee() { Id = 5, FirstName = "Katie", LastName = "Calvillo" };
-            Employee employee6 = new Employee() { Id = 6, FirstName = "Kelly", LastName = "Ventre" };
-            Employee employee7 = new Employee() { Id = 7, FirstName = "John", LastName = "Sandin" };
-            Employee employee8 = new Employee() { Id = 8, FirstName = "Joe", LastName = "Cataldi" };
-            Employee employee9 = new Employee() { Id = 9, FirstName = "Lyra", LastName = "Starns" };
-            Employee employee10 = new Employee() { Id = 10, FirstName = "Joe", LastName = "Commisso" };
-            Employee employee11 = new Employee() { Id = 11, FirstName = "Matthew", LastName = "Lommel" };
-            
-            List<Employee> employeeList = new List<Employee>(); //Creates a list of employees
-            employeeList.Add(employee1); //Adds each employee specified above to the employeeList list
-            employeeList.Add(employee2);
-            employeeList.Add(employee3);
-            employeeList.Add(employee4);
-            employeeList.Add(employee5);
-            employeeList.Add(employee6);
-            employeeList.Add(employee7);
-            employeeList.Add(employee8);
-            employeeList.Add(employee9);
-            employeeList.Add(employee10);
-            employeeList.Add(employee11);
+            //Instantiates the list for all employees without having the create individual variables and then adding them to the list:
+            List<Employee> employeeList = new List<Employee> {
+                new Employee {Id = 1, FirstName = "Joe", LastName = "Smith"},
+                new Employee {Id = 2, FirstName = "Sarah", LastName = "Holsey" },
+                new Employee {Id = 3, FirstName = "Lili", LastName = "Trenchard" },
+                new Employee {Id = 4, FirstName = "Marian", LastName = "Cronen" },
+                new Employee {Id = 5, FirstName = "Katie", LastName = "Calvillo" },
+                new Employee {Id = 6, FirstName = "Kelly", LastName = "Ventre" },
+                new Employee {Id = 7, FirstName = "John", LastName = "Sandin" },
+                new Employee {Id = 8, FirstName = "Joe", LastName = "Cataldi" },
+                new Employee {Id = 9, FirstName = "Lyra", LastName = "Starns" },
+                new Employee {Id = 10, FirstName = "Joe", LastName = "Commisso" },
+                new Employee {Id = 11, FirstName = "Matthew", LastName = "Lommel" },
+            };
 
-            //foreach (Employee employee in employeeList) //Prints out the first name of all employees of the employeeList
-            //{
-            //    Console.WriteLine("All first names: " + employee.FirstName);
-            //}
+            //Employee employee1 = new Employee() { Id = 1, FirstName = "Joe", LastName = "Smith" };
+            //Employee employee2 = new Employee() { Id = 2, FirstName = "Sarah", LastName = "Holsey" };
+            //Employee employee3 = new Employee() { Id = 3, FirstName = "Lili", LastName = "Trenchard" };
+            //Employee employee4 = new Employee() { Id = 4, FirstName = "Marian", LastName = "Cronen" };
+            //Employee employee5 = new Employee() { Id = 5, FirstName = "Katie", LastName = "Calvillo" };
+            //Employee employee6 = new Employee() { Id = 6, FirstName = "Kelly", LastName = "Ventre" };
+            //Employee employee7 = new Employee() { Id = 7, FirstName = "John", LastName = "Sandin" };
+            //Employee employee8 = new Employee() { Id = 8, FirstName = "Joe", LastName = "Cataldi" };
+            //Employee employee9 = new Employee() { Id = 9, FirstName = "Lyra", LastName = "Starns" };
+            //Employee employee10 = new Employee() { Id = 10, FirstName = "Joe", LastName = "Commisso" };
+            //Employee employee11 = new Employee() { Id = 11, FirstName = "Matthew", LastName = "Lommel" };
+
+            //List<Employee> employeeList = new List<Employee>(); //Creates a list of employees
+            //employeeList.Add(employee1); //Adds each employee specified above to the employeeList list
+            //employeeList.Add(employee2);
+            //employeeList.Add(employee3);
+            //employeeList.Add(employee4);
+            //employeeList.Add(employee5);
+            //employeeList.Add(employee6);
+            //employeeList.Add(employee7);
+            //employeeList.Add(employee8);
+            //employeeList.Add(employee9);
+            //employeeList.Add(employee10);
+            //employeeList.Add(employee11);
+
+            foreach (Employee employee in employeeList) //Prints out the first name of all employees of the employeeList
+            {
+                Console.WriteLine("All first names: " + employee.FirstName);
+            }
 
             List<Employee> employeeList2 = new List<Employee>(); //Creates an empty list named employeeList2 with data type Employee refering to the Employee class
-             foreach (Employee employee in employeeList) //Using a foreach loop, adds all employees with the first name “Joe” to the list "employeeList2"
+            foreach (Employee employee in employeeList) //Using a foreach loop, adds all employees with the first name “Joe” to the list "employeeList2"
             {
                 if (employee.FirstName == "Joe")
                 {
                     employeeList2.Add(employee);
                 }
+            }
+
+            foreach (Employee employee in employeeList2)
+            {
+                Console.WriteLine(employee.Id); //Prints out the contents of employeeList2, showing only the ids
             }
 
             List<Employee> employeeListLambda = employeeList.Where(x => x.FirstName == "Joe").ToList(); //Creates a new list of all employees with the first name “Joe” using a lambda expression as opposed to a foreach loop as used above
