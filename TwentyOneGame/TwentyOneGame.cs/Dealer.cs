de2a0930d20a69974477e6e694f7ec4110bac895
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-17 07:45:49 -0800

    Updated with writing to file

--- TwentyOneGame/TwentyOneGame.cs/Dealer.cs
+++ TwentyOneGame/TwentyOneGame.cs/Dealer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,13 @@ namespace TwentyOneGame.cs
                                           //inside of the Deal() method we are writing what we are dealing
         {
             Hand.Add(Deck.Cards.First()); //we are adding a card to the hand that passed in
+            string card = String.Format(Deck.Cards.First().ToString() + "\n"); //every time a card is dealt, we log that into our log file
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\zsuzsi\Documents\logs\log.txt", true)) //we're dealing with unmanaged code, so everything should be disposed off to clean memory
+            {                                                                                             //"true" means that yes, I want to append to log.txt, as opposed to creating a new file = then write false
+                file.WriteLine(card); //file here refers to file in the using statement
+                                      //card = we want to write whatever is inside the card variable, more specifically inside Format() to the file, so every card that is dealt to the player and the dealer
+            }//once this line is hit it will be all cleaned up by the memory manager
             Console.WriteLine(Deck.Cards.First().ToString() + "\n"); //and then we write to the console which card that was
             Deck.Cards.RemoveAt(0);
         }
--- TwentyOneGame/TwentyOneGame.cs/Program.cs
+++ TwentyOneGame/TwentyOneGame.cs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO; //this namespace is to use the file class so we can write to textfiles
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace TwentyOneGame.cs
     {
         static void Main(string[] args)
         {
+            //string text = "Here is some text.";
+            //File.WriteAllText(@"C:\Users\zsuzsi\Documents\Logs\log.txt", text); //if the log.txt doesn't exist, it will create
+                                                                               //we are writing the text to the log.txt file
+                                                                               //WriteAllText takes one parameter, which is the filepath
+                                                                               //@ means write this string exactly as is without escape characters, if you actually want escape characters use \\
+            //read back the file we just wrote:
+            //string text = File.ReadAllText(@"C:\Users\zsuzsi\Documents\Logs\log.txt");
+
             Console.WriteLine("Welcome to the Grand Hotel and Casino! What is your name?");
             string playerName = Console.ReadLine();
             Console.WriteLine("How much money did you bring today?");
