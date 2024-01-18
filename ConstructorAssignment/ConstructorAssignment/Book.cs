7937698b07c54f6da4b9b8f34ad2dfce88b8cd01
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-18 06:58:08 -0800

    Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Book
+    {
+        public Book (string title) : this (title, 14) //creates constructor that inherits from the previous constructor, if users don't provide a reading time, assign 14 (days) automatically
+        {
+        }
+
+        public Book (string title, int readingTime) //creates constructor with 2 parameters, title and reading time
+        {
+            Title = title;
+            ReadingTime = readingTime;
+        }
+        public string Title { get; set; } //sets properties Title and ReadingTime
+        public int ReadingTime { get; set; }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+
+    //Assignment instructions:
+    //Create a const variable.
+    //Create a variable using the keyword “var.”
+    //Chain two constructors together.
+
+    class Program
+    {
+        static void Main()
+        {
+            const string bookClubName = "Our Bookclub"; //creates a constant variable, using constant here because the name of the bookclub won't change
+            var newBook = "Rooted in Hope"; //creates a variable without declaring the datatype, using the var keyword
+
+            Console.WriteLine("{0} is reading: {1}.", bookClubName, newBook); //prints to the console
+            Console.ReadLine(); //prevents the console window from closing automatically
+        }
+    }
+}
