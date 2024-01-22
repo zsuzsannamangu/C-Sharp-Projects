9a8bb543e429fe44f95a17f786e6d4a20fd74f6f
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-01-22 12:26:13 -0800

    Updated with unsubscribe

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities db = new NewsletterEntities()) //initiatates the NewsletterEntities object and passed in our connection string, so now we have access to the database
+            {
+                var signups = db.SignUps.Where(x => x.Removed == null).ToList(); //we map our db object to a View model so we don't pass private information to the View
+                                                                                 //SignUps will be only those where the Removed property equals to null, so we don't display the name of those that unsubscribed
+                                                                                 //db has a property SignUps, which represents all the records in our db. This property is in Newsletter.Context.cs
+                //you can also filter with LINQ:
+                //var signups = (from c in db.SignUps
+                //               where c.Removed == null
+                //               select c).ToList();
+
+                var signupVms = new List<SignupVm>(); //we create a new list of ViewModels
+                foreach (var signup in signups) //we map the ViewModels from the model to the ViewModel
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms); //we pass that list to the View()
+            }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities db = new NewsletterEntities()) //creates connection to db
+            {
+                var signup = db.SignUps.Find(Id); //find the wanted record by the Find() method by the given primary key values
+                signup.Removed = DateTime.Now; //we change the property Removed, we are assigning it the current date and time
+                db.SaveChanges(); //we now have to update the Removed property
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -12,8 +12,6 @@ namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;
-                                            Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public ActionResult Index()
         {
             return View();
@@ -31,66 +29,101 @@ namespace NewsletterAppMVC.Controllers
             }
             else
             {
-                //save the form content in the database, we'll ADO.NET for this, so first create database with a table in the SQL Server Object Explorer
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
-                                        (@FirstName, @LastName, @EmailAddress)";
-                //the using statement will help close our connection, to prevent memory leaks
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                //we are mapping the 3 parameters to a new signup object
+
+                using (NewsletterEntities db = new NewsletterEntities())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
-
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
+                    var signup = new SignUp(); //creates new SignUp object
+                    signup.FirstName = firstName; //we map the properties for this object to the parameters that came in
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+                    db.SignUps.Add(signup); //we add it to the db
+                    db.SaveChanges(); //until the SaveChanges() method nothing is saved to the db
                 }
+
+
+                ////save the form content in the database, we'll ADO.NET for this, so first create database with a table in the SQL Server Object Explorer
+                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                //                        (@FirstName, @LastName, @EmailAddress)";
+                ////the using statement will help close our connection, to prevent memory leaks
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+                //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                //    command.Parameters["@FirstName"].Value = firstName;
+                //    command.Parameters["@LastName"].Value = lastName;
+                //    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                //    connection.Open();
+                //    command.ExecuteNonQuery();
+                //    connection.Close();
+                //}
 
                 return View("Success"); //we will now add a Success view page
             }
         }
         
-        public ActionResult Admin()
-        {
-            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress from Signups";
-            List<NewsletterSignup> signups = new List<NewsletterSignup>(); //initalize it as an empty list
+        //public ActionResult Admin()
+        //{
+        //    //Entity Framework syntax:
+        //    //Instantiates the NewsletterEntities object, using a "using" system so the connection is automatically closed when done
 
-            using (SqlConnection connection = new SqlConnection(connectionString)) //we open the connection and wrap it in a using statement
-            {
-                SqlCommand command = new SqlCommand(queryString, connection); //generate our command
-                connection.Open(); //open connection
+        //    using (NewsletterEntities db = new NewsletterEntities()) //initiatates the NewsletterEntities object and passed in our connection string, so now we have access to the database
+        //    {
+        //        var signups = db.SignUps; //we map our db object to a View model so we don't pass private information to the View
+        //                                  //db has a property SignUps, which represents all the records in our db. This property is in Newsletter.Context.cs
+        //        var signupVms = new List<SignupVm>(); //we create a new list of ViewModels
+        //        foreach (var signup in signups) //we map the ViewModels from the model to the ViewModel
+        //        {
+        //            var signupVm = new SignupVm();
+        //            signupVm.FirstName = signup.FirstName;
+        //            signupVm.LastName = signup.LastName;
+        //            signupVm.EmailAddress = signup.EmailAddress;
+        //            signupVms.Add(signupVm);
+        //        }
 
-                SqlDataReader reader = command.ExecuteReader(); //we create a data reader to read the information
+        //        return View(signupVms); //we pass that list to the View()
+        //    }
 
-                while (reader.Read())
-                {
-                    var signup = new NewsletterSignup(); //we declare a new object
-                    signup.Id = Convert.ToInt32(reader["Id"]); //we are assigning the properties to the incoming values
-                    signup.FirstName = reader["FirstName"].ToString();
-                    signup.FirstName = reader["LastName"].ToString();
-                    signup.EmailAddress = reader["EmailAddress"].ToString();
-                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
-                    signups.Add(signup); //we add our newsletter signup to our list
-                }
-            }
+            //the above does exactly what the code below would do, but the above is using Entity Framework, the code below is using ADO.NET to create dB calls
+
+            //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress from Signups";
+            //List<NewsletterSignup> signups = new List<NewsletterSignup>(); //initalize it as an empty list
+
+            //using (SqlConnection connection = new SqlConnection(connectionString)) //we open the connection and wrap it in a using statement
+            //{
+            //    SqlCommand command = new SqlCommand(queryString, connection); //generate our command
+            //    connection.Open(); //open connection
+
+            //    SqlDataReader reader = command.ExecuteReader(); //we create a data reader to read the information
+
+            //    while (reader.Read())
+            //    {
+            //        var signup = new NewsletterSignup(); //we declare a new object
+            //        signup.Id = Convert.ToInt32(reader["Id"]); //we are assigning the properties to the incoming values
+            //        signup.FirstName = reader["FirstName"].ToString();
+            //        signup.FirstName = reader["LastName"].ToString();
+            //        signup.EmailAddress = reader["EmailAddress"].ToString();
+            //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+            //        signups.Add(signup); //we add our newsletter signup to our list
+            //    }
+            //}
             //a common practice is to map your dB objects to a ViewModel in the controller:
-            var signupVms = new List<SignupVm>(); //we create a list
-            foreach (var signup in signups) //loop through all the signups
-            {
-                var signupVm = new SignupVm();
-                signupVm.FirstName = signup.FirstName; //we are mapping properties
-                signupVm.LastName = signup.LastName;
-                signupVm.EmailAddress = signup.EmailAddress;
-                signupVms.Add(signupVm);//we add it to our list
-            }
+            //var signupVms = new List<SignupVm>(); //we create a list
+            //foreach (var signup in signups) //loop through all the signups
+            //{
+            //    var signupVm = new SignupVm();
+            //    signupVm.FirstName = signup.FirstName; //we are mapping properties
+            //    signupVm.LastName = signup.LastName;
+            //    signupVm.EmailAddress = signup.EmailAddress;
+            //    signupVms.Add(signupVm);//we add it to our list
+            //}
 
-            return View(signupVms);
-        }
+            //return View(signupVms);
+        //}
 
     }
 }
